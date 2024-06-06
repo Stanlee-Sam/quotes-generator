ec5c24139b24f83ece2f3898add5307c8f29c884
stanleyamunze03@gmail.com <stanleyamunze03@gmail.com>
2024-06-06 19:30:05 +0300

    this commit will add new files to the repository

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+// export default App;
+import { useState, useEffect } from "react";
+import { ThreeDots } from "react-loader-spinner";
+
+const App = () => {
+  const [quote, setQuote] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.slip.advice);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const fetchNewQuote = () => {
+    setIsLoading(true);
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data.slip.advice);
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <>
+      <h1>Quotes</h1>
+      <div className="quote">
+        <h3>{quote}</h3>
+      </div>
+      <div>
+        {isLoading ? (
+          <ThreeDots color="#00BFFF" height={80} width={80} />
+        ) : null}
+      </div>
+      <div className="btn">
+        <button onClick={fetchNewQuote}>New Quote</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
